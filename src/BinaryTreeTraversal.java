0ff2b5fc164ae60497acfe6d42924a2bfb079c48
Agboniro Esther Jacinta <agbonirojacinta@gmail.com>
2022-08-13 16:58:42 +0100

    learn

---
+++ src/BinaryTreeTraversal.java
@@ -0,0 +1,17 @@
+import javax.swing.tree.TreeNode;
+import java.util.Map;
+import java.util.Set;
+
+public class BinaryTreeTraversal {
+    //using DFS
+
+        Map<Integer, Map<Integer, Set<Integer>>> myMap;
+
+       void solve(TreeNode curr, int col, int row){
+           if (curr==null) return;
+
+//           myMap.get(col).get(row).add(curr.get);
+//                    [col][row].insert(curr->val)
+       }
+
+}
